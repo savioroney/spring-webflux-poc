dbea37d585b285e2d08b22d071a2eb774a954452
savio.soares <savio.soares@pizzahut.com>
2020-10-24 16:54:26 -0300

    initial commit

---
+++ src/main/java/com/spring/webflux/SpringWebfluxPocApplication.java
@@ -0,0 +1,13 @@
+package com.spring.webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebfluxPocApplication.class, args);
+	}
+
+}
